7f174d2ca9f0b6fe50ec5f91f03f4817d04865b6
Clannad-sakamichi <sakamichi@foxmail.com>
2017-04-27 17:09:30 +0800

    代码优化，增加注释

--- js/sim.js
+++ js/sim.js
@@ -2,9 +2,9 @@
  * Created by sakamichi on 2017/4/26.
  */
 $(document).ready(function() {
-    var purple = 0.0413;
-    var blue = 0.412;
-    var count = 0;
+    var purple = 0.0413;//四星总期望
+    var blue = 0.412;//三星期望
+    var count = 0;//计算保底
 
     $("#card").click(function(){
        getRandom();
@@ -16,20 +16,21 @@ $(document).ready(function() {
     });
 
     function getRandom() {
-        var rand;
-        var t = 0.205;
-        var y = 0.096;
-        var total;
+        var rand;//随机量
+        var t = 0.205;              //武器常数值
+        var y = 0.096;              //圣痕常数值
+        var total;                  //累计次数
+        var pic;                    //决定展示的图片
         $("#count").html(function(i,origText){
-            total=parseInt(origText)+1;
-            return parseInt(origText)+1;
+            total=parseInt(origText)+1;//获取累计次数并累计一次
+            return total;
         });
-        if(total%10==0&&total!=1){
+        if(total%10==1&&total!=1){            //清空图片显示
             for(var i=1;i<11;i++){
                 $("#"+i).attr("srcset","").html();
             }
         }
-        if (count == 9) {
+        if (count == 9) {           //触发保底
             count = 0;
             rand = Math.random();
             if (rand < t) {
@@ -37,30 +38,27 @@ $(document).ready(function() {
                 $("#result").html(function(i,origText){
                     return origText + "保底up内武器<br\>";
                 });
-                //out.append("\n保底up内武器\n");
-                //rt.getWeapon(true,4);
             }
             else if (rand < (t + 3 * y)) {
                 //up内圣痕
                 $("#result").html(function(i,origText){
                     return origText + "保底up内圣痕<br\>";
                 });
-                //out.append("\n保底up内圣痕\n");
             }
             else {
                 //up外
                 $("#result").html(function(i,origText){
                     return origText + "保底up外<br\>";
                 });
-                //out.append("\n保底up外\n");
             }
-            $("#10").attr("srcset","img/4.jpg").html();
+            $("#"+total%10).attr("srcset","img/4.jpg").html();
             return;
         }
         rand = Math.random();
         if (rand <= purple) {
             //保底外
-            $("#"+total%10).attr("srcset","img/1.gif").html();
+            //$("#"+total%10).attr("srcset","img/1.gif").html();
+            pic=4;
             count = 0;
             rand = Math.random();
             if (rand < t) {
@@ -68,21 +66,18 @@ $(document).ready(function() {
                 $("#result").html(function(i,origText){
                     return origText + "非保底up内武器<br\>";
                 });
-                //out.append("\n非保底up内武器\n");
             }
             else if (rand < (t + 3 * y)) {
                 //up内圣痕
                 $("#result").html(function(i,origText){
                     return origText + "非保底up内圣痕<br\>";
                 });
-                //out.append("\n非保底up内圣痕\n");
             }
             else {
                 //up外
                 $("#result").html(function(i,origText){
                     return origText + "非保底up外<br\>";
                 });
-                //out.append("\n非保底up外\n");
             }
         }
         else if (rand <= ( blue + purple)) {
@@ -91,8 +86,8 @@ $(document).ready(function() {
             $("#result").html(function(i,origText){
                 return origText + "三星 ";
             });
-            $("#"+total%10).attr("srcset","img/3.jpg").html();
-            //out.append("三星 ");
+            //$("#"+total%10).attr("srcset","img/3.jpg").html();
+            pic=3;
         }
         else {
             //出二星
@@ -100,8 +95,9 @@ $(document).ready(function() {
             $("#result").html(function(i,origText){
                 return origText + "二星 ";
             });
-            $("#"+total%10).attr("srcset","img/2.jpg").html();
-            //out.append("二星 ");
+            //$("#"+total%10).attr("srcset","img/2.jpg").html();
+            pic=2;
         }
+        $("#"+total%10).attr("srcset","img/"+pic+".jpg").html();
     }
 });
