73de26fc6e39b31b013941bf1059ecba01c54c26
Clannad-sakamichi <sakamichi@foxmail.com>
2017-04-27 18:51:25 +0800

    Update sim.js

--- js/sim.js
+++ js/sim.js
@@ -25,6 +25,12 @@ $(document).ready(function() {
             total=parseInt(origText)+1;//获取累计次数并累计一次
             return total;
         });
+        $("#diamond").html(function(i,origText){
+            return parseInt(origText)+280;
+        });
+        $("#price").html(function(i,origText){
+            return parseFloat(origText)+22.4;
+        });
         if(total%10==1&&total!=1){            //清空图片显示
             for(var i=0;i<10;i++){
                 $("#"+i).attr("srcset","").html();
@@ -52,11 +58,15 @@ $(document).ready(function() {
                 });
             }
             $("#"+total%10).attr("srcset","img/4.jpg").html();
+            $("#purple").html(function(i,origText){
+                return parseInt(origText)+1;
+            });
             return;
         }
         rand = Math.random();
         if (rand <= purple) {
             //保底外
+            //$("#"+total%10).attr("srcset","img/1.gif").html();
             pic=4;
             count = 0;
             rand = Math.random();
@@ -78,6 +88,9 @@ $(document).ready(function() {
                     return origText + "非保底up外<br\>";
                 });
             }
+            $("#purple").html(function(i,origText){
+                return parseInt(origText)+1;
+            });
         }
         else if (rand <= ( blue + purple)) {
             //出三星
